e700ee787bfe4c37907ef63483b7f8750d16247d
Kepler <harveyhellewell10@gmail.com>
2025-10-28 13:48:21 +0000

    Improved Closest Hitbox Detection on Arrow Projectiles

--- src/game/server/tf/tf_projectile_arrow.cpp
+++ src/game/server/tf/tf_projectile_arrow.cpp
@@ -822,7 +822,7 @@ void CTFProjectile_Arrow::ArrowTouch( CBaseEntity *pOther )
 
 		// Intense, but extremely accurate:
 		AngleVectors( GetAbsAngles(), &forward );
-		start = GetAbsOrigin() + forward*16;
+		start = GetAbsOrigin();
 		for ( int i = 0; i < set->numhitboxes; i++ )
 		{
 			mstudiobbox_t *pbox = set->pHitbox( i );
@@ -832,8 +832,12 @@ void CTFProjectile_Arrow::ArrowTouch( CBaseEntity *pOther )
 			Ray_t ray;
 			ray.Init( start, position );
 			trace_t tr;
-			IntersectRayWithBox( ray, position+pbox->bbmin, position+pbox->bbmax, 0.f, &tr );
-			float dist = tr.endpos.DistTo( start );
+			IntersectRayWithOBB( ray, position, angles, pbox->bbmin, pbox->bbmax, 0.f, &tr );
+			
+			// We want to calculate closest distance of the arrows trajectory to a hitbox
+			// Instead of just the closest distance of the arrows position to a hitbox
+			// For better hit detection
+			float dist = ( tr.endpos - start ).Cross( vel ).Length() / vel.Length();
 
 			if ( dist < closest_dist )
 			{
